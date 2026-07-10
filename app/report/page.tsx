1f37080a7c2efcce8d7c06eb11ca060628f8b694
Sai Shravan <saishravan384@gmail.com>
2026-07-10 22:00:28 +0530

    feat: setup report route

---
+++ app/report/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold">Spark report</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ea illum
+        laboriosam neque sit consequatur minus incidunt eveniet voluptatum
+        recusandae quisquam perferendis magni itaque, inventore, facere quia
+        molestiae? Quibusdam, nesciunt!
+      </p>
+    </div>
+  )
+}
